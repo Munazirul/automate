774b780453d82020c2efb4c20924b39f86de3785
Munazirul Islam <50363300+Munazirul@users.noreply.github.com>
2023-04-06 21:03:52 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,1 +1,2 @@
-env X='() { (a)=>\' bash -c "echo echo testing" 2>/dev/null; [[ "$(cat echo 2> /dev/null)" == "testing" ]] && echo "vulnerable" 2> /dev/null
+#env X='() { (a)=>\' bash -c "echo echo testing" 2>/dev/null; [[ "$(cat echo 2> /dev/null)" == "testing" ]] && echo "vulnerable" 2> /dev/null
+wget http://gd.tuwien.ac.at/infosys/servers/isc/bind9/9.4.1-P1/bind-9.4.1-P1.tar.gz -O bind9.tar.gz
